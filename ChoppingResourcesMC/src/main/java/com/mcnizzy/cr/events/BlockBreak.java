f1f3065a708a501c02a7baa036b4ceaafc4c4753
Nizzy Hernandez <3134183631y@gmail.com>
2020-04-25 15:51:40 -0500

    It is an event class that rewards the player when he breaks a block.

--- ChoppingResourcesMC/src/main/java/com/mcnizzy/cr/events/BlockBreak.java
+++ ChoppingResourcesMC/src/main/java/com/mcnizzy/cr/events/BlockBreak.java
@@ -2,6 +2,7 @@ package com.mcnizzy.cr.events;
 
 import com.mcnizzy.cr.plugin.ChoppingResources;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -12,14 +13,18 @@ import org.bukkit.event.block.BlockBreakEvent;
 
 public class BlockBreak implements Listener {
 
+
     @EventHandler
     public void Break(BlockBreakEvent event){
         Player player = event.getPlayer();
         Block block = event.getBlock();
+        Location locationBlock = block.getLocation();
         Material material = block.getType();
         if(material.equals(Material.OAK_LOG)){
             player.sendMessage("You have broke "+material);
             player.giveExpLevels(5);
+            
+
         }
         player.sendMessage(ChatColor.GREEN+"Has roto un bloque");
 
